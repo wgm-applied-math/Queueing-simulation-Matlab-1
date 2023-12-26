6d1e9c3dbf14ca73b9c6cb484c27c708763e825b
Garrett <mitchenerg@cofc.edu>
2023-12-26 22:03:46 +0000

    Starting to add documentation

--- sort_by.m
+++ sort_by.m
@@ -1,4 +1,9 @@
 function [res, p] = sort_by(v, f)
+% sort_by  Sort a cell array based on the value of a function applied to
+% each item
+%
+% [res, p] = sort_by(v, f) Sort v, giving the result vector res, and
+% permutation p.
     u = zeros(size(v));
     for j = 1:size(v, 2)
         u(j) = f(v{j});
