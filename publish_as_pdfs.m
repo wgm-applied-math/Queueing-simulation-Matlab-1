3a52d558e934a1617f4e3c62fe246a1f4ce5e0ca
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2024-01-09 11:27:38 -0500

    Update publish_as_pdfs.m
    Added comments for students.

--- publish_as_pdfs.m
+++ publish_as_pdfs.m
@@ -1,5 +1,8 @@
 % Publish all files *.m as PDF files.
 
+% You generally won't need to use this script.
+% It produces a PDF printout of each .m file and saves it in a directory named html/
+
 m_files = dir("*.m");
 for j = 1:size(m_files, 1)
     in_file = m_files(j).name;
