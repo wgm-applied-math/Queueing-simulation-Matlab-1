512e2abb84419a00368efbbb1aa6b71ee4756ae2
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2024-03-01 12:03:37 -0500

    Clean up test_ServiceQueue
    Naming convention changes, and use of run_until instead of an explicit loop.

--- test_ServiceQueue.m
+++ test_ServiceQueue.m
@@ -1,15 +1,12 @@
-function q = test_ServiceQueue(max_time)
+function q = test_ServiceQueue(MaxTime)
     % test_ServiceQueue Basic test of the ServiceQueue class.
     %
-    % q = test_ServiceQueue(max_time) - Schedule one customer to arrive at
-    % time 1, then run the queue until its internal clock passes max_time.
-    % The default for max_time is 100.
+    % q = test_ServiceQueue(MaxTime) - Schedule one customer to arrive at
+    % time 1, then run the queue until its internal clock passes MaxTime.
     arguments
-        max_time = 100.0;
+        MaxTime = 100.0;
     end
     q = ServiceQueue(LogInterval=1, NumServers=2);
     q.schedule_event(Arrival(1, Customer(1)));
-    while q.Time < max_time
-        handle_next_event(q);
-    end
+    run_until(q, MaxTime);
 end
